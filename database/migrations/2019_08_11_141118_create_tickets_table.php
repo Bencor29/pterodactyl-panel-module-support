9747b8dfa54dd5efca4c694a9deb9173d74a18d4
Bencor29 <Bencordu29@gmail.com>
2019-08-11 14:41:35 +0200

    Added: Database structure

---
+++ database/migrations/2019_08_11_141118_create_tickets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('subject');
+            $table->integer('user_id')->unsigned();
+            $table->integer('server_id')->unsigned()->nullable();
+            $table->integer('admin_id')->unsigned()->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('server_id')->references('id')->on('servers');
+            $table->foreign('admin_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}

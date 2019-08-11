414894604c4019510c8fb4d0aea3f926c89f4c4b
Bencor29 <Bencordu29@gmail.com>
2019-08-11 23:54:41 +0200

    Added: Ticket's server relation

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -3,8 +3,17 @@
 namespace Pterodactyl\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Pterodactyl\Models\Server;
 
 class Ticket extends Model
 {
-    //
+    /**
+     * Gets information for the server associated with this allocation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function server()
+    {
+        return $this->belongsTo(Server::class);
+    }
 }
